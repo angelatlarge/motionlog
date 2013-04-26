ec371521f5f9270fe2995faa9d23e4fca415a369
Kirill Shklovsky <kirill.shklovsky@gmail.com>
2013-04-26 15:23:01 -0400

    Now have white icons produced automatically

--- image_src/addbb.sh
+++ image_src/addbb.sh
@@ -1,27 +1,38 @@
-TEMPFILENAME=temporary.eps
+TEMPFILENAME=temporary_addbb.eps
 
 if [ $# -lt 1 ] ; then
-	echo "No arguments provided"
-	exit 1 ;
+#	Using standard input and standard output
+	cat > $TEMPFILENAME
+	INFILENAME=$TEMPFILENAME
+	OUTFILENAME=/dev/stdout
 elif [ $# -lt 2 ] ; then
+#	One argument privided, use it as input, print to stdout
+	INFILENAME=$1
+	OUTFILENAME='/dev/stdout'
+elif [ "$1" = "$2" ]; then
+#	Two arguments that are the same
 	cp $1 $TEMPFILENAME
 	INFILENAME=$TEMPFILENAME
-	OUTFILENAME=$1
+	OUTFILENAME=$2
 else
+#	Two different arguments
 	INFILENAME=$1
 	OUTFILENAME=$2
 fi
 # Low res bounding box
-LORESBOUNDINGBOX=`grep 'AI._Cropmarks' $INFILENAME | awk '{for(i=2;i<=NF;i++){printf "%d ", $i}; printf "\n"}'`
+LORESBOUNDINGBOX=`cat $INFILENAME | grep 'AI._Cropmarks' | awk '{for(i=2;i<=NF;i++){printf "%d ", $i}; printf "\n"}'`
 # For high-res bounding box
-HIRESBOUNDINGBOX=`grep 'AI._Cropmarks' $INFILENAME | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}'`
+HIRESBOUNDINGBOX=`cat $INFILENAME | grep 'AI._Cropmarks'  | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}'`
 #~ cat $INFILENAME.eps | sed '/%%BoundingBox/c '"%%BoundingBox: $LORESBOUNDINGBOX\n%%HiResBoundingBox: $HIRESBOUNDINGBOX" > out.eps
+#~ echo cat $INFILENAME
+#~ cat $INFILENAME
 cat $INFILENAME \
 	| sed \
 		-e '/%%HiResBoundingBox/d' \
 		-e '/%%BoundingBox/c '"%%BoundingBox: $LORESBOUNDINGBOX\n%%HiResBoundingBox: $HIRESBOUNDINGBOX" \
 	> $OUTFILENAME
 
-if test $# -lt 2 ; then
+# [ $# -gt 1 ] && 
+if [ "$1" = "$2" ]; then
 rm -f $TEMPFILENAME
 fi
---
+++ image_src/to_white.sh
@@ -0,0 +1,38 @@
+TEMPFILENAME=temporary_towhite.eps
+
+if [ $# -lt 1 ] ; then
+#	Using standard input and standard output
+	cat > $TEMPFILENAME
+	INFILENAME=$TEMPFILENAME
+	OUTFILENAME=/dev/stdout
+elif [ $# -lt 2 ] ; then
+#	One argument privided, use it as input, print to stdout
+	INFILENAME=$1
+	OUTFILENAME='/dev/stdout'
+elif [ "$1" = "$2" ]; then
+#	Two arguments that are the same
+	cp $1 $TEMPFILENAME
+	INFILENAME=$TEMPFILENAME
+	OUTFILENAME=$2
+else
+#	Two different arguments
+	INFILENAME=$1
+	OUTFILENAME=$2
+fi
+# Low res bounding box
+LORESBOUNDINGBOX=`cat $INFILENAME | grep 'AI._Cropmarks' | awk '{for(i=2;i<=NF;i++){printf "%d ", $i}; printf "\n"}'`
+# For high-res bounding box
+HIRESBOUNDINGBOX=`cat $INFILENAME | grep 'AI._Cropmarks'  | awk '{out=$2; for(i=3;i<=NF;i++){out=out" "$i}; print out}'`
+#~ cat $INFILENAME.eps | sed '/%%BoundingBox/c '"%%BoundingBox: $LORESBOUNDINGBOX\n%%HiResBoundingBox: $HIRESBOUNDINGBOX" > out.eps
+#~ echo cat $INFILENAME
+#~ cat $INFILENAME
+cat $INFILENAME \
+	| sed \
+		-e '/0.698039 0.67451 0.639216 0.741176 0.137255 0.121569 0.12549 XA/c 0 0 0 0 1 1 1 XA'	\
+		-e '/0.698039 0.67451 0.639216 0.741176 0.137255 0.121569 0.12549 Xa/c 0 0 0 0 1 1 1 Xa'	\
+	> $OUTFILENAME
+
+# [ $# -gt 1 ] && 
+if [ "$1" = "$2" ]; then
+rm -f $TEMPFILENAME
+fi
